9bebb777bafe9afac5f58f2463a8a978fb293475
auroraautonoa <44324714+auroraautonoa@users.noreply.github.com>
2020-06-11 16:43:05 +0200

    prijava i registracija

---
+++ prijava.php
@@ -0,0 +1,19 @@
+<?php require_once 'header.php'; ?>
+
+    <div class="div-main" >
+        <div class="div-table">
+        <table>
+			<td> username: </td>
+			<td><input type="text" name="txtUsername" /></td>
+		</tr>
+		<tr>
+			<td> password: </td>
+			<td> <input type="password" name="txtPassword" /> </td>
+		</tr>
+    </table>	
+    <br> &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
+    <button class="button" id="btnPrijava" type="submit">PRIJAVI SE</button>	
+        </div>
+    </div>
+
+<?php require_once 'footer.php';?>
