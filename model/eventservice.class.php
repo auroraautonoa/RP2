f9347897bce0cb32fb7ce290c90401c78cc6c4d6
mamisla <48299179+mamisla@users.noreply.github.com>
2020-07-06 17:52:05 +0200

    model update

--- model/eventservice.class.php
+++ model/eventservice.class.php
@@ -13,7 +13,7 @@ class EventService{
         $st->execute();
 
         while ($row = $st->fetch())
-            $events[] = new Event ($row['id'], $row['autor'], $row['dolazi'], , $row['zanima'], $row['mjesto'], $row['kategorija'], $row['vrijeme_pocetak'], $row['vrijeme_kraj'], $row['datum_pocetak'], $row['datum_kraj'], $row['title'], $row['opis']);
+            $events[] = new Event ($row['id'], $row['autor'], $row['dolazi'], $row['zanima'], $row['mjesto'], $row['kategorija'], $row['vrijeme_pocetak'], $row['vrijeme_kraj'], $row['datum_pocetak'], $row['datum_kraj'], $row['title'], $row['opis']);
     
         return $events;
     }
@@ -30,4 +30,36 @@ class EventService{
         
         return $users;
     }
+
+    public function getAllComments($id_event){
+        $comments = [];
+
+        $db = DB::getConnection();
+        $st = $db->prepare('SELECT * FROM komentari WHERE id_event:=id_event ORDER BY vrijeme_objave');
+        $st->execute();
+
+        while ($row = $st->fetch())
+            $comments[] = new Comment($row['id'], $row['id_user'], $row['id_event'], $row['opis'], $row['zvjezdice'], $row['vrijeme_objave']);
+    
+        return $comments;
+    }
+
+    public function getEventTitle($id_event){
+        $db = DB::getConnection();
+		$st = $db->prepare('SELECT * FROM events WHERE id=:id_event');
+        $st->execute( );
+        
+        $row = $st->fetch();
+
+        return $row['title'];
+    }
+
+    public function sendComment($id_user, $id_event, $comment, $zvjezdice){
+        date_default_timezone_set('Europe/Paris');
+        $date = date('Y-m-d H:i:s', time());
+        $db = DB::getConnection();
+        $st = $db->prepare( 'INSERT INTO komentari(id_user, id_event, vrijeme_objave, opis, zvjezdice) VALUES (:id_user, :id_event, :vrijeme_objave, :opis, :zvjezdice)' );
+        $st->execute(array('id_user' => $id_user, 'id_event' => $id_event, 'vrijeme_objave' => $date, 'opis' => $comment, 'zvjezdice' => $zvjezdice));
+    }
+
 }
