9c556e7923395eef2cf3ac13ee19213ae1ac1a75
mamisla <48299179+mamisla@users.noreply.github.com>
2020-07-10 14:08:48 +0200

    add_event change

--- view/add_event.php
+++ view/add_event.php
@@ -7,8 +7,7 @@
 		<tr>
 			<td> Ime eventa: </td>
 			<td><input type="text" name="naslov" /></td>
-        </tr>
-        <tr>
+
 			<td> Kategorija: </td>
 			<td><input type="text" name="kategorija" /></td>
 		</tr>
@@ -19,30 +18,27 @@
 		<tr>
 			<td> Mjesto održavanja: </td>
 			<td><input type="text" name="mjesto" /></td>
-		</tr>
-		<tr>
+		
 			<td> Grad održavanja: </td>
 			<td><input type="text" name="grad" /></td>
 		</tr>
 		<tr>
 			<td> Datum početka događaja: </td>
 			<td><input type="date" name="datum_pocetak" /></td>
-		</tr>
-		<tr>
+
 			<td> Datum kraja događaja: </td>
 			<td><input type="date" name="datum_kraj" /></td>
 		</tr>
 		<tr>
 			<td> Vrijeme početka događaja: </td>
 			<td><input type="time" name="vrijeme_pocetak" /></td>
-		</tr>
-		<tr>
+
 			<td> Vrijeme kraja događaja: </td>
 			<td><input type="time" name="vrijeme_kraj" /></td>
 		</tr>
     	</table>
     <br> &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
-    <button class="button" id="btnPrijava" type="submit">KREIRAJ DOGAĐAJ</button>	
+    <button class="button" id="btnPrijava" type="submit" style="margin-left:6cm">KREIRAJ DOGAĐAJ</button>	
     	</div>
     </div>
 	</form>
